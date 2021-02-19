c6602a2c6b5615e1c1809066bc900bb39fa60a19
ShocK1999 <przemek.salek@amorki.pl>
2021-02-19 11:42:27 +0100

    exe file

---
+++ venv/Scripts/cxfreeze-quickstart-script.py
@@ -0,0 +1,12 @@
+#!D:\Users\USER\PycharmProjects\VisitsAdder\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'cx-Freeze==6.5.3','console_scripts','cxfreeze-quickstart'
+__requires__ = 'cx-Freeze==6.5.3'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('cx-Freeze==6.5.3', 'console_scripts', 'cxfreeze-quickstart')()
+    )
